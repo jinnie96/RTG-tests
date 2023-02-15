13661086fc3a9911e63f57e06d1716c312619598
Karandeep <jinnie1996@hotmail.com>
2023-02-15 01:01:32 -0800

    added before each

--- tests/RTG-Tests.spec.js
+++ tests/RTG-Tests.spec.js
@@ -1,18 +1,21 @@
 const { test, expect } = require('@playwright/test')
 
+test.beforeEach(async ( {page}, testInfo) => {
+  await page.goto('https://remember-the-grapes.herokuapp.com/');
+  await page.getByRole('link', { name: 'Log In' }).click();
+  await page.getByRole('button', { name: 'Demo User' }).click();
+})
+
+
 test.describe('Authentication', () => {
 
   test('should allow user to log in as demo', async ({ page }) => {
-    await page.goto('https://remember-the-grapes.herokuapp.com/');
-    await page.getByRole('link', { name: 'Log In' }).click();
-    await page.getByRole('button', { name: 'Demo User' }).click();
-    await expect(page).toHaveURL('https://remember-the-grapes.herokuapp.com/');
+    // await page.locator('button:below(#submitBtn))').click()
+    const logout = await page.locator('text=Logout')
+    await expect(logout).toBeTruthy()
   });
 
   test('should allow user to sign out', async ({ page }) => {
-    await page.goto('https://remember-the-grapes.herokuapp.com/');
-    await page.getByRole('link', { name: 'Log In' }).click();
-    await page.getByRole('button', { name: 'Demo User' }).click();
     await page.getByRole('button', { name: 'Logout' }).click();
     const headline = await page.innerText('#smartAppBusy')
     await expect(headline).toBe('The smart to-do app for busy people.')
@@ -22,9 +25,6 @@ test.describe('Authentication', () => {
 test.describe('Tasks', () => {
 
   test('should allow user to view pending tasks', async ({ page }) => {
-    await page.goto('https://remember-the-grapes.herokuapp.com/');
-    await page.getByRole('link', { name: 'Log In' }).click();
-    await page.getByRole('button', { name: 'Demo User' }).click();
     await page.getByText('tst2').click();
     const task = await page.locator('text=Delete Task')
     await expect(task).toBeTruthy()
@@ -35,9 +35,6 @@ test.describe('Tasks', () => {
 test.describe('Lists', () => {
 
   test('should allow user to create new lists', async ({ page }) => {
-    await page.goto('https://remember-the-grapes.herokuapp.com/');
-    await page.getByRole('link', { name: 'Log In' }).click();
-    await page.getByRole('button', { name: 'Demo User' }).click();
     await page.locator('#plusfa').click();
     await page.locator('#editListInput').click();
     await page.locator('#editListInput').fill('testingList');
@@ -48,9 +45,6 @@ test.describe('Lists', () => {
 });
 
 test('should allow user to view lists', async ({ page }) => {
-  await page.goto('https://remember-the-grapes.herokuapp.com/');
-  await page.getByRole('link', { name: 'Log In' }).click();
-  await page.getByRole('button', { name: 'Demo User' }).click();
   await page.locator('#rightArrow').click();
   await page.getByRole('paragraph').filter({ hasText: 'School' }).click();
   const list = page.locator('p').filter({ hasText: 'School' })
@@ -58,9 +52,6 @@ test('should allow user to view lists', async ({ page }) => {
 });
 
 test('should allow user to edit lists', async ({ page }) => {
-  await page.goto('https://remember-the-grapes.herokuapp.com/');
-  await page.getByRole('link', { name: 'Log In' }).click();
-  await page.getByRole('button', { name: 'Demo User' }).click();
   await page.locator('#rightArrow').click();
   await page.locator('#fa-edit').first().click();
   await page.getByRole('textbox').nth(1).click();
